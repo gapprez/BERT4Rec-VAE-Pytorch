d4732522ff7486fd420c9c8e675f13f54ac484e2
Víctor Escudero <v.escudero99@gmail.com>
2025-03-23 04:55:35 +0100

    Updated main
    Took 1 hour 47 minutes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from aggregation import Average, BordaCount
-from dataset import SteamRSDataset, MINDRSDataset, MovieLensRSDataset, ML1m
-from grouping import FCMWithPCCGrouping, KNNGrouping, ContentBasedPCCGrouping
+from aggregation import Average, AGGREGATION_METHODS
+from grouping import GROUPING_STRATEGIES
 from tqdm import tqdm
 
 from dataloaders import dataloader_factory
@@ -26,9 +25,9 @@ def test():
     trainer.test()
 
 
-def set_experiment_description():
+def set_experiment_description(grouping):
     args.experiment_description = f"test_{args.dataset_code}_{args.grouping_code}"
-    if is_similarity_grouping(args.grouping_code):
+    if grouping.is_similarity():
         args.experiment_description = f"{args.experiment_description}_{args.similarity_threshold}"
     else:
         args.experiment_description = f"{args.experiment_description}_{args.n_clusters}"
@@ -39,73 +38,25 @@ def set_experiment_description():
 
 
 def test_all_config():
-    for aggregation_code in tqdm([None, Average.code(), BordaCount.code()], desc='Testing all configurations'):
+    for aggregation_code in tqdm([None] + list(AGGREGATION_METHODS.keys()), desc='Testing all configurations'):
         args.do_aggregation = aggregation_code is not None
         args.aggregation_code = aggregation_code if args.do_aggregation else Average.code()
 
-        for grouping_code in [FCMWithPCCGrouping.code(), ContentBasedPCCGrouping.code(), KNNGrouping.code()]:
-            args.grouping_code = grouping_code
-            is_similarity = is_similarity_grouping(args.grouping_code)
-            n_clusters_or_sim = get_n_clusters_similarity_list()
+        for grouping in GROUPING_STRATEGIES.values():
+            args.grouping_code = grouping.code()
+            is_similarity = grouping.is_similarity()
+            n_clusters_or_sim = grouping.get_sim_list(args.dataset_code) if is_similarity else grouping.get_n_clusters_list(args.dataset_code)
 
             for clust_sim in n_clusters_or_sim:
                 args.n_clusters = clust_sim if not is_similarity else args.n_clusters
                 args.similarity_threshold = clust_sim if is_similarity else args.similarity_threshold
 
-                for group_size in get_group_size_list():
+                for group_size in grouping.get_group_size_list(args.dataset_code):
                     args.group_size = group_size
-                    set_experiment_description()
+                    set_experiment_description(grouping)
                     args.trainer_code = get_trainer_code()
 
                     test()
-
-
-def get_n_clusters_similarity_list():
-    dataset_value = args.dataset_code
-    grouping_value = args.grouping_code
-
-    # Match the tuple (dataset_value, grouping_value)
-    if dataset_value in [SteamRSDataset.code(), MINDRSDataset.code()] and grouping_value == FCMWithPCCGrouping.code():
-        return [10, 20]
-
-    elif dataset_value in [MovieLensRSDataset.code(), ML1m.code()] and grouping_value == FCMWithPCCGrouping.code():
-        return [5, 10, 20]
-
-    elif dataset_value in [SteamRSDataset.code(), MINDRSDataset.code()] and grouping_value == KNNGrouping.code():
-        return [1.4, 1.6]
-
-    elif grouping_value == ContentBasedPCCGrouping.code():
-        return [0.8, 0.9]
-
-    elif dataset_value in [MovieLensRSDataset.code(), ML1m.code()] and grouping_value == KNNGrouping.code():
-        return [1.4, 1.6]
-
-    else:
-        raise Exception(f"Error getting n_clusters/similarity list: ({dataset_value}, {grouping_value})")
-
-
-def get_group_size_list():
-    # Match the tuple (dataset_value, grouping_value)
-    dataset_value = args.dataset_code
-    grouping_value = args.grouping_code
-
-    if dataset_value in [SteamRSDataset.code(), MINDRSDataset.code()] and \
-            grouping_value in [KNNGrouping.code(), FCMWithPCCGrouping.code()]:
-        return [50, 100]
-
-    elif dataset_value in [SteamRSDataset.code(), MINDRSDataset.code()] and \
-            grouping_value == ContentBasedPCCGrouping.code():
-        return [5, 10]
-
-    elif dataset_value in [MovieLensRSDataset.code(), ML1m.code()]:
-        return [5, 10]
-
-    else:
-        raise Exception(f"Error getting group_size list: ({dataset_value}, {grouping_value})")
-
-
-def is_similarity_grouping(grouping_code):
-    return grouping_code in [KNNGrouping.code(), ContentBasedPCCGrouping.code()]
 
 
 def get_trainer_code():
